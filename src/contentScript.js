980f6cb51eef9ebde6ce534258fbbcc329f841c6
Harry Zhang <zh.harry@outlook.com>
2020-02-15 18:10:40 -0800

    ngrok

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const API_GOOGLE_NLP = "https://language.googleapis.com/v1beta2/documents:classifyText?key=AIzaSyCi6QuyfyYaU0N6kpN_A5OcmqIZ3uEB5cg";
-const API_LOCAL_CO2E = "http://localhost/OneDrive/Programming/TH2020/co2e.php";
+const API_LOCAL_CO2E = "http://c19f391b.ngrok.io/OneDrive/Programming/TH2020/co2e.php";
 
 
 /* AMAZON BACKGROUND INFORMATION COLLECTION */
