48d9b58b7a5dee10660b644e0a62ff1711a7fffc
estelledes <estellechung333@gmail.com>
2020-02-16 08:26:18 -0800

    updated dash again

--- public/chart-data.js
+++ public/chart-data.js
@@ -1,52 +1,77 @@
-var selected = "carbon";
-var colour;
-var bcColour;
-
-
-if (selected === "carbon") {
-    colour = "rgb(121,200,166)";
-    bgColour = "rgb(121,200,166,0.5)";
-} else if (selected === "energy") {
-    colour = "rgb(255,200,69)";
-    bgColour = "rgb(255,200,69,0.5)";
-} else if (selected === "water") {
-    colour = "rgb(101,193,214)";
-    bgColour = "rgb(101,193,214,0.5)";
+function init2() {
+    carbon();
 }
 
-var ctx = document.getElementById('co2-chart');
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: bgColour,
-            borderColor: colour,
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
+document.addEventListener('DOMContentLoaded', init2, false);
+
+
+
+document.getElementById("type-co2").addEventListener("click", carbon);
+document.getElementById("type-energy").addEventListener("click", energy);
+document.getElementById("type-water").addEventListener("click", water);
+
+var selected = "carbon";
+var colour;
+var bgColour;
+var graphLabel;
+
+function carbon() {
+    colour = "rgb(121,200,166)";
+    bgColour = "rgb(121,200,166,0.5)";
+    graphLabel = "Carbon Usage";
+    drawGraph();
+}
+
+function energy() {
+    colour = "rgb(255,200,69)";
+    bgColour = "rgb(255,200,69,0.5)";
+    graphLabel = "Energy Usage";
+    drawGraph();
+}
+
+function water() {
+    colour = "rgb(101,193,214)";
+    bgColour = "rgb(101,193,214,0.5)";
+    graphLabel = "Water Usage";
+    drawGraph();
+}
+
+
+function drawGraph() {
+    var ctx = document.getElementById('co2-chart');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: graphLabel,
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: bgColour,
+                borderColor: colour,
+                borderWidth: 1
             }]
         },
-        legend: {
-            display: false
-        },
-        tooltips: {
-            callbacks: {
-                label: function(tooltipItem) {
-                    return tooltipItem.yLabel;
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            },
+            legend: {
+                display: false
+            },
+            tooltips: {
+                callbacks: {
+                    label: function(tooltipItem) {
+                        return tooltipItem.yLabel;
+                    }
                 }
             }
         }
-    }
-});
+    });
+}
 
 var ctx2 = document.getElementById('category-chart');
 var myDoughnutChart = new Chart(ctx2, {
