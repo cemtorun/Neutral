2645b00dce77d45485fb12eee4a7e1c0b1d7b456
Harry Zhang <zh.harry@outlook.com>
2020-03-29 21:45:13 -0400

    Added close and dashboard button (#16)

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -4,4 +4,13 @@
     popupHandler.RunForCurrentTab();
 
     chrome.storage.onChanged.addListener(popupHandler.DataChangeCallback);
+
+    document.getElementById("action-close").addEventListener("click", (e) => {
+        e.preventDefault();
+        window.close();
+    })
+    document.getElementById("action-dashboard").addEventListener("click", (e) => {
+        e.preventDefault();
+        chrome.runtime.openOptionsPage();
+    })
 })();
