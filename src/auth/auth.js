96b5d4c78508908d03bcbc5674c192a2db1c4636
jackyliao123 <me@jackyliao.me>
2020-04-25 22:56:06 -0400

    added actual authentication

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -1,38 +1,61 @@
-function userLogin(identifier, password, callback) {
+"use strict";
+var auth_url_base = "neutral-dev.tk:1337"
+var auth_url_register = auth_url_base + "/auth/local/register";
+var auth_url_login = auth_url_base + "/auth/local";
 
+async function jsonAPICall(endpoint, content) {
+	return await new Promise(function(resolve, reject) {
+		var xhr = new XMLHttpRequest();
+		xhr.open("POST", auth_url_login, true);
+		xhr.onreadystatechange = function() {
+			if(xhr.readyState == 4) {
+				var response = JSON.parse(xhr.responseText);
+				if(xhr.status == 200) {
+					resolve(response);
+				} else {
+					reject(response);
+				}
+			}
+		}
+		xhr.sendRequestHeader("Content-Type", "application/json;charset=UTF-8");
+		xhr.send(JSON.stringify(content));
+	});
+}
+
+async function userLogin(identifier, password) {
+	var ret = await jsonAPICall(auth_url_login, {
+		identifier: identifier,
+		password: password
+	});
+	delete ret.user.purchases;
+	chrome.storage.local.set({"login": ret});
+	return ret;
 }
 
 function userLogout() {
-
+	chrome.storage.local.remove(["login"]);
 }
 
-function userRegister(username, email, password, callback) {
-
+function userRegister(username, email, password) {
+	var ret = await jsonAPICall(auth_url_register, {
+		username: username,
+		email: email,
+		password: password
+	});
+	delete ret.user.purchases;
+	chrome.storage.local.set({"login": ret});
+	return ret;
 }
 
-function isLoggedIn() {
-    return true;
+async function getUser() {
+	return await new Promise(function(resolve, reject) {
+		chrome.storage.local.get(["login"], function(res) {
+			resolve(res.jwt);
+		});
+	});
 }
 
-function getUser() {
-    return {
-        "jwt": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNTg3ODU2NTE5LCJleHAiOjE2MTkzOTI1MTl9.Bm_8EPmH2zxSu7pl4kQvW7SlbSovfD_L1G9XQpFu6Hc",
-        "user": {
-            "id": 2,
-            "username": "newuser1",
-            "email": "neutral_test001@zharry.ca",
-            "provider": "local",
-            "confirmed": true,
-            "blocked": null,
-            "role": {
-                "id": 1,
-                "name": "Authenticated",
-                "description": "Default role given to authenticated user.",
-                "type": "authenticated"
-            },
-            "created_at": "2020-04-25T16:39:24.000Z",
-            "updated_at": "2020-04-25T16:39:24.000Z",
-            "purchases": []
-        }
-    }
+async function isLoggedIn() {
+	return !!(await getUser());
 }
+
